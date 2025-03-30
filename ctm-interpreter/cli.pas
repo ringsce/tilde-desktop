90507ee77c3a74e58da4c000b91540da1e2b1eab
Pedro Dias Vicente (Gleentech Solutions) <pedrolvicente@gmail.com>
2025-03-30 11:28:59 +0100

    adding cli

--- ctm-interpreter/cli.pas
+++ ctm-interpreter/cli.pas
@@ -37,13 +37,21 @@ end;
 
 procedure ShowHelp;
 begin
-  Writeln('Usage: git <repo_url> [destination]');
-  Writeln('Example: git https://github.com/ringsce/tilde-desktop.git');
-  Writeln('         git https://github.com/ringsce/tilde-desktop.git my-folder');
+  Writeln('Usage: ctminterpreter <command> [arguments]');
   Writeln;
-  Writeln('Options:');
-  Writeln('  --help      Show this help message and exit');
+  Writeln('Commands:');
+  Writeln('  git <repo_url> [destination]  Clone a Git repository');
+  Writeln('  --sync                        Pull latest changes from the remote repo');
+  Writeln('  --commit "<message>"          Commit all changes with a message');
+  Writeln('  --build-from-source           Build the project using Makefile in "tilde-desktop"');
+  Writeln('  --reset                       Hard reset the repository & sync again');
+  Writeln('  --help                        Show this help message and exit');
   Halt(0);
+end;
+
+procedure SyncRepo;
+begin
+  RunCommand('cd tilde-desktop && git pull');
 end;
 
 procedure CloneGitRepo(const RepoURL, Destination: string);
@@ -69,6 +77,33 @@ begin
   GitProcess.Free;
 end;
 
+procedure CommitChanges(const Message: string);
+begin
+  RunCommand('git add .');
+  RunCommand('git commit -m "' + Message + '"');
+  RunCommand('git push -u --force');
+end;
+
+procedure BuildFromSource;
+begin
+  Writeln('Building project in "tilde-desktop" using Makefile...');
+  RunCommand('cd tilde-desktop && make');
+  Writeln('Build completed!');
+
+  // Run --sync after build
+  SyncRepo;
+end;
+
+procedure ResetProject;
+begin
+  Writeln('Resetting project in "tilde-desktop"...');
+  RunCommand('cd tilde-desktop && git reset --hard && git clean -fd');
+  Writeln('Project reset completed!');
+
+  // Run --sync after reset
+  SyncRepo;
+end;
+
 procedure RunCLI;
 var
   RepoPath: string;
@@ -87,11 +122,54 @@ begin
   end
   else
   begin
-    Writeln('Usage: cli --sync');
+    Writeln('Usage: ctminterpreter --sync');
   end;
 end;
 
+var
+  RepoURL, Destination, CommitMessage: string;
 begin
-  CloneGitRepo('https://github.com/ringsce/tilde-desktop.git', 'tilde-desktop');
+  if (ParamCount = 1) and (ParamStr(1) = '--help') then
+    ShowHelp;
+
+  if (ParamCount = 1) and (ParamStr(1) = '--sync') then
+  begin
+    SyncRepo;
+    Halt(0);
+  end;
+
+  if (ParamCount = 2) and (ParamStr(1) = '--commit') then
+  begin
+    CommitMessage := ParamStr(2);
+    CommitChanges(CommitMessage);
+    Halt(0);
+  end;
+
+  if (ParamCount = 1) and (ParamStr(1) = '--build-from-source') then
+  begin
+    BuildFromSource;
+    Halt(0);
+  end;
+
+   if (ParamCount = 1) and (ParamStr(1) = '--reset') then
+  begin
+    ResetProject;
+    Halt(0);
+  end;
+
+  if ParamCount < 1 then
+  begin
+    Writeln('Error: Missing arguments. Use --help for usage.');
+    Halt(1);
+  end;
+
+  RepoURL := ParamStr(1);
+
+  if ParamCount >= 2 then
+    Destination := ParamStr(2)
+  else
+    Destination := ExtractFileName(ChangeFileExt(RepoURL, ''));
+
+  CloneGitRepo(RepoURL, Destination);
 end.
 
